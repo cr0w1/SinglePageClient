f36475361c7e8e09a0281e1f5d82c85ddd3e39af
cr0w1 <clebsonjose30@gmail.com>
2022-05-28 00:50:00 -0300

    sigle page aplication

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'animate.css';
+// import '../styles/styles/elementor/main.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
